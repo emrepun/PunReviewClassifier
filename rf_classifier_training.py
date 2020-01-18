e1a8174376d3332c69e831aad1aa172105f162ee
emrepun <emrehavan@hotmail.com>
2020-01-18 11:21:22 +0100

    Add rfc training

---
+++ rf_classifier_training.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import pickle
+import json
+
+dataset = pd.read_csv('cleared_reviews.csv')
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(dataset["review"].values.astype('U')).toarray()
+y = dataset["is_positive"]
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(n_estimators = 501,
+                            criterion = 'entropy')
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('Score: ', model.score(X_test, y_test))
